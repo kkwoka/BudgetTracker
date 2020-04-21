27ccbb78a5ce551215ad4d63c60664fab1270333
Kendra Kwoka <kendrakwoka@gmail.com>
2020-04-21 18:16:29 -0500

    to push to heroku

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -2,9 +2,9 @@ const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 
 const config = {
-    entry: "./public/index",
+    entry: "./index",
     output: {
-      path: __dirname + "/public/dist",
+      path: __dirname + "/dist",
       filename: "bundle.js"
     },
     mode: "production",
@@ -28,7 +28,7 @@ const config = {
             {
               src: path.resolve(
                 __dirname,
-                "public/icons/icon-512x512.png"
+                "icons/icon-512x512.png"
                 ),
               // the plugin will generate an image for each size
               // included in the size array
